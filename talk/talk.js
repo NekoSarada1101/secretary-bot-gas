4cbd15c8bca9d8ce24b5478d427714e0ff5cd58c
Sarada <ryoyukikazu11@gmail.com>
2019-12-13 21:30:12 +0900

    add:秘書の雑談機能追加
    作成済みの雑談機能を追加

---
+++ talk/talk.js
@@ -0,0 +1,44 @@
+//雑談bot
+function doPost(e){
+  var message = e.parameter.text;
+  var user_name = e.parameter.user_name;
+  try{
+    var url = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk";
+    var apikey = PropertiesService.getScriptProperties().getProperty('TALK_API_KEY');
+    
+    var payload =
+        {
+          "apikey" : apikey,
+          "query" : message
+        };
+    
+    var options =
+        {
+          "method" : "post",
+          "payload" : payload
+        };
+    
+    var response =  UrlFetchApp.fetch(url, options);
+    var json = JSON.parse(response.getContentText());
+    var result = json["results"][0]["reply"];
+    Logger.log("成功");
+  }catch(e){
+    Logger.log(e);
+    var result =  user_name + "さん！" + message + "と言いましたね";
+  }
+  var data = {"text": result};
+  postSlack(data);
+  return ContentService.createTextOutput();
+}
+
+function postSlack(data) {
+  var options = {
+    'method'      : 'post'              ,
+    'contentType' : 'application/json'  ,
+    "muteHttpExceptions":true,
+    'payload'     : JSON.stringify(data)
+  };
+  
+  var url = PropertiesService.getScriptProperties().getProperty('URL_TODAY');
+  UrlFetchApp.fetch(url, options);
+}
