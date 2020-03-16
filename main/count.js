b1aa609ab34298e2b306079dc7510e52bfd70888
Sarada <ryoyukikazu11@gmail.com>
2020-03-16 18:19:26 +0900

    fix:日数カウントがマイナスのものを非表示にする

--- main/count.js
+++ main/count.js
@@ -1,14 +1,14 @@
 function count() {
-    var sheet_url = SpreadsheetApp.openByUrl(PropertiesService.getScriptProperties().getProperty('SPREADSHEET_URL'));
-    var sheet_name = sheet_url.getSheetByName("カウント");
+    var spreadsheet = SpreadsheetApp.openByUrl(PropertiesService.getScriptProperties().getProperty('SPREADSHEET_URL'));
+    var sheet = spreadsheet.getSheetByName("カウント");
 
-    var max_row = sheet_name.getDataRange().getLastRow();
+    var max_row = sheet.getDataRange().getLastRow();
     var count_text = "";
 
     for (var i = 2; i <= max_row; i++) {
-        var title = sheet_name.getRange(i, 1).getValue();
-        var sheet_date = sheet_name.getRange(i, 2).getValue().split("/");
-        var up_or_down = sheet_name.getRange(i, 3).getValue();
+        var title = sheet.getRange(i, 1).getValue();
+        var sheet_date = sheet.getRange(i, 2).getValue().split("/");
+        var up_or_down = sheet.getRange(i, 3).getValue();
 
         var date = new Date(sheet_date[0], sheet_date[1] - 1, sheet_date[2]);
         var today = new Date();
@@ -19,10 +19,16 @@ function count() {
         } else if (up_or_down.match("ダウン")) {
             count_date = date.getTime() - today.getTime();
         }
+
+        if(count_date < 0){
+            continue;
+        }
+        
         var count = Math.floor(count_date / 1000 / 60 / 60 / 24);
 
         count_text += "`" + title + "` " + count + "日\n";
     }
+
     var count_json = {
         "response_type": "ephemeral",
         "attachments": [
