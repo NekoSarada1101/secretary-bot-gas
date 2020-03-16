c76c28e569fef30309efd2064e7fd61c30d5dea7
Sarada <ryoyukikazu11@gmail.com>
2020-03-16 21:28:06 +0900

    fix:スプレッドシート関連の処理を使用不可に
    タイムアウトしてしまうのでコメントアウト

--- main/connpass.js
+++ main/connpass.js
@@ -17,16 +17,26 @@ function fetchConnpassEvent() {
         var place = json["events"][i]["place"];
         var url = json["events"][i]["event_url"];
         var start = json["events"][i]["started_at"];
-        var spreadsheet = SpreadsheetApp.openById(PropertiesService.getScriptProperties().getProperty('SPREADSHEET_URL'));
-        var sheet = spreadsheet.getSheetByName('connpass');
-        var data = sheet.getDataRange().getValues();
         var event_info = { title: title, address: address, place: place, url: url, start: start };
 
+        // var spreadsheet = SpreadsheetApp.openByUrl(PropertiesService.getScriptProperties().getProperty('SPREADSHEET_URL'));
+        // var sheet = spreadsheet.getSheetByName('connpass');
+        // var max_row = sheet.getDataRange().getLastRow();
+
+        // for (var j = 1; j <= max_row; j++) {
+        // var title_entered_sheet = sheet.getRange(j,1).getValue();
+
+        // if (title === title_entered_sheet) {
+        // break;
+        // } else if (j === max_row) {
         createEventJson(event_info/*, max_row, sheet*/);
+        // }
+        // }
     }
 }
 
 function createEventJson(event_info, max_row, sheet) {
+    // sheet.getRange(max_row + 1,1).setValue(event_info["title"]);
 
     var json = {
         "text": "*" + event_info["title"] + "* \n場所：" + event_info["address"] + event_info["place"] + "\n開始日時：" + event_info["start"].substring(0, 16).replace("T", " ") + "\n" + event_info["url"]
